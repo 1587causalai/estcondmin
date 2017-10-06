c97ef5f2c9f5b3a624262e349109161004f4ac6c
HeyangGong <gonghy@mail.ustc.edu.cn>
2017-10-06 19:34:14 +0800

    add test

---
+++ tests/testthat.R
@@ -0,0 +1,10 @@
+library(testthat)
+library(estcondmin)
+
+test_that("hello, test", {
+  expect_that("a", is_a("character"))
+})
+
+test_that("gen_dat test", {
+  expect_that(gen_dat, is_a("list"))
+})
